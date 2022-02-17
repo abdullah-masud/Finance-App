f77aaa36d6e4f264ad001156461a7a8f517996d8
Abdullah Al Masud <masudctg00@gmail.com>
2022-02-17 13:54:18 +0600

    full functions code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /* Warning message function */
-function warningMessage() {
-    const warnMessage = document.getElementById('block');
+function warningMessage(itemID) {
+    const warnMessage = document.getElementById(itemID);
     warnMessage.style.display = 'block'
     warnMessage.style.color = 'red'
 }
@@ -10,7 +10,7 @@ function incomeInput() {
     const incomeInputField = document.getElementById('income-input');
     const incomePrice = parseFloat(incomeInputField.value);
     if (incomePrice < 0 || isNaN(incomePrice)) {
-        warningMessage()
+        warningMessage('block')
     } else {
         return incomePrice;
     }
@@ -28,7 +28,8 @@ function addExpenses() {
     const clothesPrice = parseFloat(clothesInputField.value);
 
     if (foodPrice < 0 || isNaN(foodPrice) || rentPrice < 0 || isNaN(rentPrice) || clothesPrice < 0) {
-        warningMessage()
+        warningMessage('block')
+
     } else {
         const total = foodPrice + rentPrice + clothesPrice;
         return total
@@ -47,17 +48,19 @@ document.getElementById('calculate-button').addEventListener('click', function()
 
     const warnMessage = document.getElementById('block');
 
-    if (total > 0) {
-        totalExpenses.innerText = total
-        warnMessage.innerText = "Enter Income to see the balance"
-    } else if (total > incomePrice) {
-        warnMessage.style.display = 'block'
-        warnMessage.style.color = 'red'
-        warnMessage.innerText = "Your expenses should be less than your income"
-    }
     if (incomePrice > total) {
         warnMessage.style.display = 'none'
         totalBalanceField.innerText = balance
+        totalExpenses.innerText = total
+    } else if (total > incomePrice) {
+        warningMessage('block')
+        warnMessage.innerText = "Your expenses should be less than your income to see the balance"
+        totalExpenses.innerText = total
+        totalBalanceField.innerText = ""
+    } else if (total > 0) {
+        totalExpenses.innerText = total
+        warnMessage.innerText = "Enter Positive Income to see the balance"
+        totalBalanceField.innerText = ""
     }
 })
 
@@ -66,29 +69,33 @@ document.getElementById('save-button').addEventListener('click', function() {
     const incomePrice = incomeInput()
 
     const savingInputField = document.getElementById('saving-input');
-    const savingValue = parseFloat(savingInputField.value);
+    const savingPercentage = parseFloat(savingInputField.value);
 
     const savingAmountField = document.getElementById('saving-amount');
-    const savings = (savingValue / 100) * incomePrice;
+    const savings = (savingPercentage / 100) * incomePrice;
 
     const remainingBalanceField = document.getElementById('remaining-balance');
 
     const totalBalanceField = document.getElementById('total-balance')
     const totalBalance = parseFloat(totalBalanceField.innerText);
 
-    const warnMessage = document.getElementById('block2');
+    const warnMessage2 = document.getElementById('block2');
     if (savings > totalBalance) {
-        warnMessage.style.display = 'block'
-        warnMessage.style.color = 'red'
+        warnMessage2.style.display = 'block'
+        warnMessage2.style.color = 'red'
         savingAmountField.innerText = "";
         remainingBalanceField.innerText = "";
-    } else {
+
+    } else if (savings < totalBalance) {
         savingAmountField.innerText = savings.toFixed(2)
         const remaining = totalBalance - savings
         remainingBalanceField.innerText = remaining.toFixed(2)
-        warnMessage.style.display = 'none'
+        warnMessage2.style.display = 'none'
     }
-    if (isNaN(savingValue) || savingValue < 0) {
+    if (isNaN(savingPercentage) || savingPercentage < 0) {
+        warnMessage2.style.display = 'block'
+        warnMessage2.style.color = 'red'
+        warnMessage2.innerText = "Enter positive number only"
         savingAmountField.innerText = "";
         remainingBalanceField.innerText = "";
     }
